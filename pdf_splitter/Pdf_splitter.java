1d6b6d2b05ab78bbfddec106250fd12156500cf4
Aditya <104798214+Advirocks82@users.noreply.github.com>
2023-02-27 03:13:32 +0530

    Add files via upload

---
+++ pdf_splitter/Pdf_splitter.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdf_splitter;
+
+public class Pdf_splitter {    
+    public static void main(String[] args){
+    Dashboard db = new Dashboard();
+    db.setVisible(true);    
+    }
+}
